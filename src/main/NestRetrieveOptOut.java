4621ce9867bd9e47b4f43079de2ddde3f39d5be5
Christopher Morrison <cmorrison91@hotmail.com>
2022-07-01 23:34:16 +0100

    started NestRetrieveOptOutClass

--- src/main/NestRetrieveOptOut.java
+++ src/main/NestRetrieveOptOut.java
@@ -4,9 +4,25 @@
 package main;
 
 /**
- * @author cmorr
+ *  * @author Christopher Morrison
+ *	Class for handling of the "request to retrieve opt outs" web service call.
+ *	The guidance for this can be found in section 3.5 of web-services-api-specification document
+ *	published by NEST.
  *
+ *	The order of actions in this process are:
+ *	- Send GET request to initiate the request (authorisation)
+ *	- 202 response = accepted
+ *	- response also includes expected time of request completion
+ *	- Send GET request for status of processing at indicated time
+ *	- 201 response = created
+ *	- Send final GET request for opt-out data
+ *	- 200 response = OK
+ * 	- body of response contains requested data
+ * 
+ * 	This class is intended to encapsulate the processing of the specific opt out request flow.
+ * 	The intention will be that the end response is saved for future reference TBD. database to use?
  */
+
 public class NestRetrieveOptOut {
 
 }
